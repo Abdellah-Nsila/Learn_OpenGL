17d27bd8325bf6fa952ba6001ace3fd6330585c0
Abdellah-Nsila <dawndawn074@gmail.com>
2025-10-23 23:33:26 +0100

    Remove build artifacts and generated files from the Learn_OpenGL project. This includes deleting CMake generated files, build directories, and binary outputs such as shaders and textures. This cleanup ensures a fresh state for the project without any residual files from previous builds.

---
+++ Core/Source/Core/Application.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "string"
+
+namespace	Core
+{
+	struct ApplicationSpecification
+	{
+		std::string	Name = "Application";
+		WindowSpecification = WindowSpec;
+	};
+
+	class Application
+	{
+		public:
+			Application(/* args */);
+			~Application();
+		private:
+			/* data */
+	};
+	
+	Application::Application(/* args */)
+	{
+	}
+	
+	Application::~Application()
+	{
+	}
+	
+	
+}
